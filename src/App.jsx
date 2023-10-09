73f70ecb9479d7dfc73849855da9e5e443dd4dc0
Manav9823 <manavbansalfddk@gmail.com>
2023-10-09 18:08:07 +0530

    intial work

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
         <button onClick = {()=> setColor("red")}
         className="outline-none px-4 py-1 rounded-full 
-        text-white shadow-lg"
+         text-white shadow-lg"
         style={{backgroundColor:"red"}}
         >Red</button>
         <button onClick = {()=> setColor("black")}
